4243fd5d040afb93946f107ae983622b0c00c5c0
Evan <93694706+ejma96@users.noreply.github.com>
2024-12-09 09:39:05 -0500

    Sign-Up CSS and Form Touch-Ups
    Sign-Up CSS and Form Touch-Ups

---
+++ Scripts/signup.js
@@ -0,0 +1,11 @@
+function submission() {
+    const firstName = document.getElementById('firstname');
+    const lastName = document.getElementById('lastname');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const familyMembers = document.getElementById('familymembers');
+    const state = document.getElementById('state');
+
+    console.log(firstName);
+    return false;
+}
