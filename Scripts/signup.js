a0eb418325198e8ae449a5a2f575e4cf341a5e45
Evan <evanjm7@icloud.com>
2024-12-12 10:17:19 -0500

    Submission Form Validation

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -1,11 +1,43 @@
-function submission() {
-    const firstName = document.getElementById('firstname').value;
-    const lastName = document.getElementById('lastname').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const familyMembers = document.getElementById('familymembers').value;
-    const state = document.getElementById('state').value;
+function signup() {
+    let firstName = document.forms['signup-form']['firstname'].value;
+    let lastName = document.forms['signup-form']['lastname'].value;
+    let email = document.forms['signup-form']['email'].value;
+    let phone = document.forms['signup-form']['phone'].value;
+    let familyMembers = document.forms['signup-form']['familymembers'].value;
+    let state = document.forms['signup-form']['state'].value;
 
-    console.log(firstName);
-    return false;
+    if (firstName == "") {
+        alert("First Name must be filled out!");
+        return false;
+
+    }
+
+    if (lastName == "") {
+        alert("Last Name must be filled out!");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Email must be filled out!");
+        return false;
+    }
+
+    if (phone == "") {
+        alert("Phone must be filled out!");
+        return false;
+    }
+
+    if (familyMembers == "0") {
+        alert("Please select your family size!");
+        return false;
+    }
+
+    if (state == "0") {
+        alert("Please select your state of residence!");
+        return false;
+    }
+
+   
+    alert("Thank you for your submission. A Vault-Tec Representative will be in touch shortly. Thank you for being prepared for the future!")
+    
 }
